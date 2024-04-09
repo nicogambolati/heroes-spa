44759693720f68e7372a51a33c1a08ba279284ca
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-09 16:57:51 -0300

    creates second router & useNavigate

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,16 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import { MarvelPage } from "../heroes/pages/MarvelPage";
-import { DcPage } from "../heroes/pages/DcPage";
-import { LoginPages } from "../auth/pages/LoginPages";
+import { LoginPages } from "../auth";
+import { HeroesRoutes } from "../heroes";
 
 export const AppRouter = () => {
   return (
-    <Routes>
-      <Route path="/*" element={<Navigate to="/marvel" />} />
-      <Route path="marvel" element={<MarvelPage />} />
-      <Route path="dc" element={<DcPage />} />
-      <Route path="login" element={<LoginPages />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="login" element={<LoginPages />} />
+
+        <Route path="/*" element={<HeroesRoutes />} />
+      </Routes>
+    </>
   );
 };
