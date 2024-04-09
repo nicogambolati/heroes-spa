1231dd41a3843e06e72469e451851c29e3fb0e2d
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-09 17:59:06 -0300

    creates getHeroById

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,5 +1,14 @@
+import { useParams, Navigate } from "react-router-dom";
+import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
+  const {id} = useParams();
+  const hero = getHeroById(id);
+
+  if (!hero) {
+    return <Navigate to="/marvel" />
+  }
+
   return (
     <h1>HeroPage</h1>
   )
