b87135886ac1337a3e94e3c1df64551b983f6c47
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-09 20:26:43 -0300

    adds sass and fixes somes styles

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -15,26 +15,26 @@ export const HeroCard = ({
   const heroImageUrl = `/assets/heroes/${id}.jpg`;
 
   return (
-    <div className="col animate__animated animate__fadeIn">
-      <div className="card">
-        <div className="row no-gutters">
-          <div className="col-4">
-            <img
-              src={`./assets/heroes/${id}.jpg`}
-              className="card-img"
-              alt={superhero}
-            />
+    <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-3 animate__animated animate__fadeIn">
+      <div className="card h-100 d-flex flex-column">
+        <img
+          src={`./assets/heroes/${id}.jpg`}
+          className="card-img-top"
+          alt={superhero}
+        />
+        <div className="card-body d-flex flex-column d-flex justify-content-between">
+          <div>
+            <h5 className="card-title">{superhero}</h5>
+            <p className="card-text">{alter_ego}</p>
+            <CharactersByHero alter_ego={alter_ego} characters={characters} />
           </div>
-          <div className="col-8">
-            <div className="card-body">
-              <h5 className="card-title">{superhero}</h5>
-              <p className="card-text">{alter_ego}</p>
-              <CharactersByHero alter_ego={alter_ego} characters={characters} />
-              <p className="card-text">
-                <small className="text-muted">{first_appearance}</small>
-              </p>
-              <Link to={`/hero/${id}`}>More...</Link>
-            </div>
+          <div>
+            <p className="card-text">
+              <small className="text-muted">{first_appearance}</small>
+            </p>
+            <Link to={`/hero/${id}`} className="btn btn-primary mt-auto">
+              More...
+            </Link>
           </div>
         </div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
-import "./styles.css";
+import "./styles.scss";
+
 import { HeroesApp } from "./HeroesApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
