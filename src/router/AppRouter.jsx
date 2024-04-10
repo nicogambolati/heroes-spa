c278d04e18f798b695b2c2f5d1ceca6ddae0e3ec
Nico Gambolati <nicolasgambolati@gmail.com>
2024-04-10 13:37:13 -0300

    creates public route

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,11 +3,19 @@ import { Routes, Route } from "react-router-dom";
 import { LoginPages } from "../auth";
 import { HeroesRoutes } from "../heroes";
 import { PrivateRoute } from "../heroes/routes/PrivateRoute";
+import { PublicRoute } from "../heroes/routes/PublicRoute";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="login" element={<LoginPages />} />
+      <Route
+        path="login"
+        element={
+          <PublicRoute>
+            <LoginPages />
+          </PublicRoute>
+        }
+      />
 
       <Route
         path="/*"
@@ -17,6 +25,9 @@ export const AppRouter = () => {
           </PrivateRoute>
         }
       />
+
+      {/* <Route path="login" element={<LoginPages />} />*/}
+      {/* <Route path="/*" element={<HeroesRoutes />} /> */}
     </Routes>
   );
 };
